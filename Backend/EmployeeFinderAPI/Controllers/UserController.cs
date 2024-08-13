6a73728db3ace4417da734ef7f692a0892a92b1e
wintheradam123 <65341319+wintheradam123@users.noreply.github.com>
2024-08-13 15:01:17 +0200

    api with basic crud added

---
+++ Backend/EmployeeFinderAPI/Controllers/UserController.cs
@@ -0,0 +1,315 @@
+﻿using Algolia.Search.Models.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Shared;
+using Shared.Helpers;
+using Shared.Models;
+
+namespace TeamFinderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("_myAllowSpecificOrigins")]
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// TODO:
+        /// DONE - Handle users who doesn't exist in Azure anymore (Been deleted)
+        /// Add authorization to only allow trusted access
+        /// Remove controllers for adding, deleting and updating users? (Check if we can update skills and solutions and still remove these controllers)
+        /// </summary>
+        private readonly Context _context;
+
+
+        private IMemoryCache _cache;
+        //private IUserService _userService;
+
+        public UserController(Context context, IMemoryCache cache /*, IUserService userService*/)
+        {
+            _context = context;
+            _cache = cache;
+            //_userService = userService;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            try
+            {
+                var users = await _context.Users
+                    .OrderBy(on => on.DisplayName)
+                    .Select(u => new User
+                    {
+                        Id = u.Id,
+                        ExternalId = u.ExternalId,
+                        DisplayName = u.DisplayName,
+                        JobTitle = u.JobTitle,
+                        OfficeLocation = u.OfficeLocation,
+                        Skills = u.Skills,
+                        Solutions = u.Solutions,
+                        UserPrincipalName = u.UserPrincipalName,
+                        //ImageSize = u.ImageSize,
+                        //ImageData = null
+                    })
+                    .ToListAsync();
+
+                    var metadata = new
+                    {
+                        users.Count
+                    };
+
+                    Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+                    return Ok(users);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error while getting users: " + e.Message);
+            }
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            try
+            {
+                if (id == 0)
+                    return BadRequest("ID is cannot be empty");
+
+                var user = await _context.Users
+                    .Select(u => new User
+                    {
+                        Id = u.Id,
+                        ExternalId = u.ExternalId,
+                        DisplayName = u.DisplayName,
+                        JobTitle = u.JobTitle,
+                        OfficeLocation = u.OfficeLocation,
+                        Skills = u.Skills,
+                        Solutions = u.Solutions,
+                        UserPrincipalName = u.UserPrincipalName,
+                        //ImageSize = u.ImageSize,
+                        //ImageData = null
+                    })
+                    .FirstOrDefaultAsync(u => u.Id == id);
+
+                if (user == null)
+                {
+                    return NotFound(id);
+                }
+
+                return user;
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error while getting user: " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Supposed to receive a list of users, checks if all the users exist in the database. If a user in the database doesn't exist in the list, it gets removed.
+        /// Should never be called, only by the scheduled retrieval of users!
+        /// </summary>
+        /// <param name="users"></param>
+        /// <returns></returns>
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<ActionResult<int>> CompareDeleteUpdateUsers(List<User> users)
+        {
+            if (users.Count < 300)
+                return BadRequest();
+
+            try
+            {
+                List<User> respUsers = new();
+                foreach (var user in _context.Users)
+                {
+                    try
+                    {
+                        var userAzureId = users.FirstOrDefault(x => x.ExternalId == user.ExternalId);
+
+                        if (userAzureId != null) continue;
+
+                        _context.Users.Remove(user);
+                        respUsers.Add(user);
+                    }
+                    catch
+                    {
+                        //Log it here!
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+
+                if (respUsers.Count > 0)
+                {
+                    return Ok(respUsers.Count);
+                }
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error while comparing users: " + e.Message);
+            }
+        }
+
+        [HttpGet("azureId/{id}")]
+        public async Task<ActionResult<User>> GetUserAzureId(int id)
+        {
+            try
+            {
+                var user = await _context.Users
+                    .Select(u => new User
+                    {
+                        Id = u.Id,
+                        ExternalId = u.ExternalId,
+                        DisplayName = u.DisplayName,
+                        JobTitle = u.JobTitle,
+                        OfficeLocation = u.OfficeLocation,
+                        Skills = u.Skills,
+                        Solutions = u.Solutions,
+                        UserPrincipalName = u.UserPrincipalName,
+                        //ImageSize = u.ImageSize,
+                        //ImageData = null
+                    })
+                    .FirstOrDefaultAsync(x => x.ExternalId == id);
+
+                if (user == null)
+                {
+                    return NotFound(id);
+                }
+
+                return user;
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error occurred while getting user: " + e.Message);
+            }
+        }
+
+        // GET: api/User/image/3771db45-af47-4c1e-ab34-dacc653f58ff
+        // return image as byte array
+        //[HttpGet("image/{azureId}")]
+        //public async Task<ActionResult<byte[]>> GetImageUserId(int externalId)
+        //{
+        //    try
+        //    {
+        //        if (!_cache.TryGetValue(externalId, out byte[] imageData))
+        //        {
+        //            var user = await _context.Users
+        //                .Select(u => new User
+        //                {
+        //                    Id = u.Id,
+        //                    ExternalId = u.ExternalId,
+        //                    ImageData = u.ImageData
+        //                })
+        //                .FirstOrDefaultAsync(x => x.ExternalId == externalId);
+
+        //            if (user == null)
+        //            {
+        //                return NotFound(externalId);
+        //            }
+
+        //            imageData = user.ImageData ?? new byte[] { 0 };
+
+        //            var cacheEntryOptions = new MemoryCacheEntryOptions()
+        //                .SetAbsoluteExpiration(TimeSpan.FromHours(2));
+
+        //            _cache.Set(externalId, imageData, cacheEntryOptions);
+        //        }
+
+        //        return File(imageData, "image/jpeg");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return BadRequest("Error occurred while getting image for user: " + e.Message);
+        //    }
+        //}
+
+        [HttpGet("clearCache")]
+        public ActionResult ClearCache()
+        {
+            try
+            {
+                if (_cache is MemoryCache concreteMemoryCache)
+                {
+                    concreteMemoryCache.Clear();
+                }
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error occurred while clearing cache: " + e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            try
+            {
+                if (id != user.Id)
+                {
+                    return BadRequest();
+                }
+
+                user.Hash = user.GetHashCode();
+
+                try
+                {
+                    _context.Update(user);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException e)
+                {
+                    if (!UserExists(id))
+                    {
+                        return NotFound();
+                    }
+
+                    throw;
+                }
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Error occurred while editing user: " + e.Message);
+            }
+        }
+
+        [HttpGet("IndexAllUsers")]
+        public async Task<IActionResult> IndexAllUsers()
+        {
+            var users = await _context.Users.Include(user => user.Solutions).Include(user => user.Skills).ToListAsync();
+
+            var algoliaUsers = AlgoliaHelperUsers.TransformToAlgolia(users);
+
+            await AlgoliaHelperUsers.Index(algoliaUsers);
+
+            return Ok();
+        }
+
+        [HttpGet("PartialIndexAllUsers")]
+        public async Task<IActionResult> PartialIndexAllUsers()
+        {
+            var users = await _context.Users.Include(user => user.Solutions).Include(user => user.Skills).ToListAsync();
+
+            var algoliaUsers = AlgoliaHelperUsers.TransformToAlgolia(users);
+
+            await AlgoliaHelperUsers.PartialUpdate(algoliaUsers);
+
+            return Ok();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
