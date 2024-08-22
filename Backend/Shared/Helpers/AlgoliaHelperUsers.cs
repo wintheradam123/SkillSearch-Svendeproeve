a99005819a4d6054baafc510c17a4c6d9f68cfa1
wintheradam123 <65341319+wintheradam123@users.noreply.github.com>
2024-08-22 14:04:26 +0200

    Revert "added empty array to algoliauser"
    This reverts commit 564990ddd877d8766128de10658e036baeda2a78.

--- Backend/Shared/Helpers/AlgoliaHelperUsers.cs
+++ Backend/Shared/Helpers/AlgoliaHelperUsers.cs
@@ -44,13 +44,13 @@ namespace Shared.Helpers
                 Solutions = user.Solutions?.Select(solution => new AlgoliaSolution()
                 {
                     SolutionName = solution.Title
-                }).ToList() ?? new List<AlgoliaSolution>(),
+                }).ToList(),
                 JobTitle = user.JobTitle,
                 OfficeLocation = user.OfficeLocation,
                 Skills = user.Skills?.Select(skill => new AlgoliaSkill
                 {
                     Tag = skill.Title
-                }).ToList() ?? new List<AlgoliaSkill>(),
+                }).ToList(),
                 UserPrincipalName = user.UserPrincipalName,
                 //HasImage = user.ImageSize != null && user.ImageSize != 0,
                 Role = user.Role
